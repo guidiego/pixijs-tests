09bc0099a8613ee9e7526e0e0e94c145c558e8e9
Guilherme Diego <guilherme.albino.francisco@gmail.com>
2022-02-26 20:01:29 +0100

    task: finish ImageTextTool
    I have abstracted the configuration to receive and format raw inputs to
    be able to receive more custom infos

--- src/scripts/imagetool.ts
+++ src/scripts/imagetool.ts
@@ -1,31 +1,69 @@
-import { Application, Sprite, Text, Texture } from "pixi.js";
+import { Application, Texture } from "pixi.js";
 import { ImageTextTool } from "./util/ImageTextTool";
+
+const SEC = 1000;
 
 const app = new Application({
   resizeTo: window,
 });
 
-app.loader.add("sprite-itens", "/assets/sprite-itens.json").load(() => {
-  const val = new ImageTextTool(
-    [
-      new Sprite(Texture.from("Apple")),
-      new Text("Test", { fill: "#FFF" }),
-      new Text("Test", { fill: "#FFF" }),
-    ],
-    { x: 100, y: 100, padding: 10 }
-  );
+const RAND_FONT_MAX = 50;
+const RAND_FONT_MIN = 10;
+const LIST_OF_RANDOM_WORLD = [
+  "game",
+  "foo",
+  "bar",
+  "deutschland",
+  "english",
+  "phaser",
+  "pixi.js",
+  "webpack",
+  "fizzfuzz",
+  "development",
+  "production",
+  "alexanderplatz",
+  "u-bahn",
+  "brazil",
+];
 
-  const val2 = new ImageTextTool(
-    [
-      new Sprite(Texture.from("Apple")),
-      new Text("Test", { fill: "#FFF" }),
-      new Text("Test", { fill: "#FFF" }),
-    ],
-    { x: 200, y: 200, padding: 10 }
-  );
+const createRandomImageText = (images, delta) => {
+  const randConfig = [
+    Math.round(Math.random() * (LIST_OF_RANDOM_WORLD.length - 1)),
+    Math.round(Math.random() * (LIST_OF_RANDOM_WORLD.length - 1)),
+    Math.round(Math.random() * (LIST_OF_RANDOM_WORLD.length - 1)),
+  ].map((val) => {
+    if (val % 2 === 0) {
+      return LIST_OF_RANDOM_WORLD[val];
+    }
 
-  app.stage.addChild(val2);
-  app.stage.addChild(val);
-});
+    return Texture.from(images[val]);
+  });
+
+  const fontSize = Math.round(RAND_FONT_MIN + Math.random() * RAND_FONT_MAX);
+  const x = Math.round(Math.random() * window.innerWidth * delta);
+  const y = Math.round(Math.random() * window.innerHeight * delta);
+
+  return new ImageTextTool(randConfig, {
+    x,
+    y,
+    padding: 10,
+    textStyle: { fontSize },
+  });
+};
+
+app.loader
+  .add("sprite-itens", "/assets/sprite-itens.json")
+  .load((_, resources) => {
+    const itens = Object.keys(resources["sprite-itens"].data.frames);
+
+    let nextMoving = 0;
+    app.ticker.add((delta) => {
+      const now = Date.now();
+      if (nextMoving <= now) {
+        nextMoving = now + SEC;
+        app.stage.addChild(createRandomImageText(itens, delta));
+      }
+    });
+  });
 
 document.body.appendChild(app.view);
--- src/scripts/util/ImageTextTool.ts
+++ src/scripts/util/ImageTextTool.ts
@@ -1,23 +1,34 @@
-import { Container, Sprite, Text } from "pixi.js";
+import { Container, Sprite, Text, Texture, TextStyle } from "pixi.js";
 
-type AllowedInTool = Text | Sprite;
+type AllowedInTool = Texture | string;
+type ResultedInTool = Sprite | Text;
 type ImageTextToolOpts = {
   x?: number;
   y?: number;
   padding?: number;
+  textStyle?: Partial<TextStyle>;
 };
 
 export class ImageTextTool extends Container {
   constructor(
     items: AllowedInTool[],
-    { x = 0, y = 0, padding = 0 }: ImageTextToolOpts = {}
+    { x = 0, y = 0, padding = 0, textStyle }: ImageTextToolOpts = {}
   ) {
     super();
-
     this.position.set(x, y);
 
-    items.forEach((item, idx) => {
-      const offset = items.reduce(
+    textStyle.fill = !textStyle.fill ? "#FFF" : textStyle.fill;
+
+    const createdItems: ResultedInTool[] = items.map((raw) => {
+      if (raw instanceof Texture) {
+        return new Sprite(raw);
+      } else {
+        return new Text(raw, textStyle);
+      }
+    });
+
+    createdItems.forEach((item, idx) => {
+      const offset = createdItems.reduce(
         (a, c, i) => (i < idx ? a + c.width + padding : a),
         0
       );
